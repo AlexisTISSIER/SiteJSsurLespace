6efe448394e112f552d6d239eefb0488b898acf2
AlexisTISSIER <tissieralexis@outlook.fr>
2024-02-09 13:48:03 +0100

    Update script.js

--- space/script.js
+++ space/script.js
@@ -7,13 +7,15 @@ async function fetchDataAndDisplay(url, elementId, label) {
       console.error(`Une erreur s'est produite lors de la récupération des données pour ${label} :`, error);
   }
 }
-// Fonction pour récuperer la data
+
+// Fonction pour récupérer les données depuis une URL
 async function fetchData(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
-// Fonction pour afficher la data choisis
+
+// Fonction pour afficher les données
 async function displayData() {
   try {
       await fetchDataAndDisplay('https://swapi.dev/api/people/', 'peopleCount', 'Nombre d\'êtres vivants recensés');
@@ -24,4 +26,29 @@ async function displayData() {
   }
 }
 
-window.onload = displayData;
+// Appel de la fonction pour afficher les données au chargement de la page
+window.onload = () => {
+  displayData();
+  displayImageFromAPI();
+};
+
+// Fonction pour récupérer et afficher une image de l'API NASA APOD
+async function displayImageFromAPI() {
+  try {
+      const maCleAPI = "bjCirviyEKCageagswN9SsdlsSfW8gdckzxaEfNZ";
+      const urlAPI = `https://api.nasa.gov/planetary/apod?api_key=${maCleAPI}`;
+
+      // Fetch de l'image depuis l'API
+      const response = await fetch(urlAPI);
+      const imageData = await response.json();
+
+      // Sélection de l'élément avec la classe "nasa"
+      const imageNASA = document.querySelector('.nasa');
+
+      // Mise à jour de l'attribut src de l'image avec l'URL de l'image de l'API
+      imageNASA.src = imageData.url;
+      imageNASA.alt = imageData.title;
+  } catch (error) {
+      console.error('Une erreur s\'est produite lors de la récupération de l\'image depuis l\'API :', error);
+  }
+}
